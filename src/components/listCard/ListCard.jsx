6d31df5f71dcf3c08bb0177a16866dc334bad923
Harun Can Ergin <114945266+haruncane@users.noreply.github.com>
2023-05-19 01:05:06 +0300

    manual update

---
+++ src/components/listCard/ListCard.jsx
@@ -0,0 +1,13 @@
+import { Movie } from "@mui/icons-material";
+import "./listCard.scss"
+
+const listCard = () => {
+  return (
+    <div className="listCard">
+        <h2>listName</h2>
+        <Movie className="centerIcon"/>
+    </div>
+  )
+}
+
+export default listCard
