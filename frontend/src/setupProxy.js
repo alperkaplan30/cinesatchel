d52aa63566ed47684cff6a97dc49da965e7f3352
Alper Kaplan <aalperkaplan30@gmail.com>
2023-07-03 02:37:02 +0300

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
       "/api",
       createProxyMiddleware({
-        target: "http://localhost:3001",
+        target: "cinesatchel-deneme-api.vercel.app",
         changeOrigin: true,
       })
     );
