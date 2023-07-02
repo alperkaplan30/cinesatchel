0e8602e09ae6a102b5108a0974e77a8b0d44b247
Alper Kaplan <aalperkaplan30@gmail.com>
2023-07-03 02:52:20 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
+app.use(express.static(dirname));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(dirname, "index.html"));
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
