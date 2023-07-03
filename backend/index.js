e7da869f9af787fd287f09891ef09c669b169936
Alper Kaplan <aalperkaplan30@gmail.com>
2023-07-03 21:01:05 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://cinesatchel-demo.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', "Content-Type");
   next();
 });
 
