536d3e84e6ef3376bb5ee8b7bc3493d82a19bd4d
Alper Kaplan <aalperkaplan30@gmail.com>
2023-07-03 20:53:24 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ mongoose
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
