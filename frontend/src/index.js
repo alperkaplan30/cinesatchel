49a5898e7dc18175e2aff4e98c52b6ff004392e7
Alper Kaplan <aalperkaplan30@gmail.com>
2023-07-04 03:32:44 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-
-
+import { AuthContextProvider } from "./context/authContext/AuthContext"
+import { VideoContextProvider } from "./context/videoContext/VideoContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <AuthContextProvider>
+      <VideoContextProvider>
+        <App />
+      </VideoContextProvider>
+    </AuthContextProvider>
+  </>
 )
