6f6584432169e52ef437042fdbf996255c159b1d
Alper Kaplan <aalperkaplan30@gmail.com>
2023-07-04 03:52:09 +0300

    Update SimilarFeed.jsx

--- frontend/src/components/similarFeed/SimilarFeed.jsx
+++ frontend/src/components/similarFeed/SimilarFeed.jsx
@@ -10,7 +10,11 @@ export default function SimilarFeed({ current }) {
     useEffect(() => {
         const getSimilars = async () => {
           try {
-            const res = await axios.get(process.env.REACT_APP_API_KEY + `api/videos/find/${id}/similars`)
+            const res = await axios.get(`${process.env.REACT_API_KEY}api/videos/find/${id}/similars` , {
+              headers: { 
+                token: "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken, 
+              }
+            });
             //console.log(res)
             setSimilars(res.data);
           } catch (err) {
